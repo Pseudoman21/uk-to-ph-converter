4a1be7eb0e947869f7f66153d3ca2ad26f406fdd
pseudoman21 <johnpaul.batusa@gmail.com>
2023-06-28 00:45:36 +0800

    Modify and add footer

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import CurrentDateTime from './components/CurrentDateTime'
 
 function App () {
   return (
-    <div className='App'>
-      <header className='App-header bg-gray-800 text-white text-2xl py-4'>
-        <CurrentDateTime />
-      </header>
-      <main className='App-body'>
-        <TimeConverter />
-      </main>
-    </div>
+    <>
+      <div className='App'>
+        <header className='App-header bg-gray-800 text-white text-2xl py-4'>
+          <CurrentDateTime />
+        </header>
+        <main className='App-body'>
+          <TimeConverter />
+        </main>
+      </div>
+      <footer className='bg-gray-800 text-gray-500 transition duration-300 ease-in-out py-5 text-center hover:text-white'>
+        <a
+          href='https://github.com/Pseudoman21'
+          target='_blank'
+          rel='noreferrer'
+        >
+          &copy; Pseudoman21, 2023
+        </a>
+      </footer>
+    </>
   )
 }
 
